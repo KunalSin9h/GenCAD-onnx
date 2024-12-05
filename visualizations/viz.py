f6886a25b3a8ad9f72ad1c1803afe0ac90df3b15
ferdous-alam <alam.92@osu.edu>
2024-12-05 17:49:13 -0500

    outline

---
+++ visualizations/viz.py
@@ -0,0 +1,27 @@
+# put your code here
+
+
+class VizMesh: 
+    def __init__(self, input):
+        self.input = input
+
+
+
+class VizPointCloud: 
+    def __init__(self, input):
+        self.input = input
+
+
+class VizSTL: 
+    def __init__(self, input):
+        self.input = input
+
+
+class VizVoxel: 
+    def __init__(self, input):
+        self.input = input
+
+
+class VizImage: 
+    def __init__(self, input):
+        self.input = input
